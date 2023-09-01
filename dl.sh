f8988a1688484d4e99f9ff22267790e1082069de
真真夜夜 <92699499+Mieluoxxx@users.noreply.github.com>
2023-09-01 08:04:32 -0500

    Create dl.sh

---
+++ dl.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# 检查poetry是否安装
+if ! command -v poetry &> /dev/null; then
+
+  # 询问是否安装
+  read -p "检测到未安装poetry，是否安装poetry？[Y/n]" ans
+  ans=${ans:-Y} # 默认为Y
+  
+  if [ "$ans" = "Y" ] || [ "$ans" = "y" ]; then
+    # 执行安装poetry的命令
+    pip install poetry
+	poetry config virtualenvs.in-project true
+  else
+    echo "未安装poetry，脚本退出。"
+    exit 1
+  fi
+fi
+
+# 检查传入参数
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <project_name>"
+  exit 1
+fi
+
+project_name=$1
+
+# 定义目录结构
+structure=(
+  "README.md"
+  "data/raw"
+  "data/processed"
+  "src/models"
+  "src/utils"
+  "src/preprocessing.py"
+  "src/train.py"
+  "src/evaluate.py" 
+  "src/predict.py"
+  "saved_models/"
+  "logs/"
+  "results/"  
+)
+
+# 生成项目目录
+mkdir $project_name
+cd $project_name
+
+
+# 初始化poetry
+poetry init
+
+
+for item in ${structure[@]}; do
+  if [[ $item == *"/"* ]]; then
+    # 包含/,生成目录
+    mkdir -p $item
+  else
+    # 不包含/,生成文件
+    touch $item 
+  fi  
+done
+
+# 自动在README.md写入目录结构
+echo "
+|- README.md             项目说明
+|- data/                 数据目录
+    |- raw/              原始数据
+    |- processed/        预处理后的数据
+|- src/                  源代码目录  
+    |- models/           模型定义
+    |- utils/            各种utility函数
+    |- preprocessing.py  数据预处理
+    |- train.py          训练脚本
+    |- evaluate.py       评估脚本
+    |- predict.py        预测脚本
+|- saved_models/         保存的模型
+|- logs/                 日志目录  
+|- results/              结果目录
+|- pyproject.toml        环境依赖 
+" > README.md
+
+# 选择项目框架
+echo "请选择项目框架:"
+select framework in "机器学习" "Tensorflow" "PyTorch" "Jax"; do
+  case $framework in
+    "机器学习")
+      # 安装scikit-learn等机器学习包
+	  poetry add pandas matplotlib scikit-learn
+      ;;
+    "Tensorflow")
+      # 安装TensorFlow
+      read -p "请选择Tensorflow版本(2.6或最新):" tf_version
+	  poetry add tensorflow
+      ;;  
+    "PyTorch")
+      # 安装PyTorch
+	  read -p "请选择PyTorch版本(推荐1.13.1或2.0.1):" torch_version
+	    poetry add torch
+      ;;
+    "Jax")
+      # 安装Jax
+      ;;
+  esac
+  break
+done
+
+echo "Project $project_name generated successfully!"
