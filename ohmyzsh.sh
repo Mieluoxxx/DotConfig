b58a99f58aa48bb48604570ea0b5796a2993b6bb
moguw <weiyiding0@gmail.com>
2023-10-28 12:09:08 +0800

    del[10.28] clear project

--- ohmyzsh.sh
+++
@@ -1,7 +0,0 @@
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-
-echo '\n#######################################################################
-请修改.zshrc中plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-completions)\n以开启zsh的高亮与自动补全'
